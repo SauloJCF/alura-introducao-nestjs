d75bc4156642a77da2b240dc3ac88b3896f22548
Saulo.Figueiredo <saulojcf@gmail.com>
2025-07-26 14:53:19 -0300

    Atualização de usuário

--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -18,4 +18,22 @@ export class UsuarioRepository {
         const existeUsuario = this.usuarios.some((usuario: UsuarioEntity) => usuario.email === email);
         return existeUsuario;
     }
+
+    async atualizar(id: string, novosDados: Partial<UsuarioEntity>) {
+        const possivelUsuario = this.usuarios.find(usuario => usuario.id === id);
+
+        if (!possivelUsuario) {
+            throw new Error('Usuário não encontrado!');
+        }
+
+        Object.entries(novosDados).forEach(([chave, valor]) => {
+            if (chave === 'id') {
+                return;
+            }
+
+            possivelUsuario[chave] = valor;
+        });
+
+        return possivelUsuario;
+    }
 }
